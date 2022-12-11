387cec8c36941a87ca8db1af1248ffae8dac6628
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2022-12-11 22:45:44 +0400

    +1

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -15,6 +15,39 @@ const products_reducer = (state, action) => {
       return { ...state, isSidebarOpen: true };
     case SIDEBAR_CLOSE:
       return { ...state, isSidebarOpen: false };
+    case GET_PRODUCTS_BEGIN:
+      return { ...state, products_loading: true };
+    case GET_PRODUCTS_SUCCESS:
+      const featured_products = action.payload.filter((product) => {
+        return product.featured === true;
+      });
+      return {
+        ...state,
+        products_loading: false,
+        products: action.payload,
+        featured_products,
+      };
+    case GET_PRODUCTS_ERROR:
+      return { ...state, products_error: true, products_loading: false };
+    case GET_SINGLE_PRODUCT_BEGIN:
+      return {
+        ...state,
+        single_product_loading: true,
+        single_product_error: false,
+      };
+    case GET_SINGLE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        single_product_loading: false,
+        single_product_error: false,
+        single_product: action.payload,
+      };
+    case GET_SINGLE_PRODUCT_ERROR:
+      return {
+        ...state,
+        single_product_error: true,
+        single_product_loading: false,
+      };
     default:
       return state;
   }
